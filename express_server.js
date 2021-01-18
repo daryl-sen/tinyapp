4ffe43da89e5133f5960c396233eec9d76806fb1
daryl <tsdaryl@gmail.com>
2021-01-18 12:54:09 -0800

    installed ejs

--- express_server.js
+++ express_server.js
@@ -7,9 +7,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+/* *** ROUTES ******************************************** */
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
+
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
