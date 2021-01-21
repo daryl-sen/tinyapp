50a9e52e48dac5d33444d012df534af38542743c
daryl <tsdaryl@gmail.com>
2021-01-21 14:18:38 -0800

    working second chai assertion test

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,22 +4,29 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };
 
 describe('getUserByEmail', function() {
+
   it('should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers);
     const expectedOutput = "userRandomID";
-    // Write your assert statement here
     assert.strictEqual(user.id, expectedOutput);
   });
+
+  it('should return false if the email is not in use', function() {
+    const user = getUserByEmail("null@example.com", testUsers);
+    const expectedOutput = false;
+    assert.strictEqual(user, expectedOutput);
+  });
+
 });
