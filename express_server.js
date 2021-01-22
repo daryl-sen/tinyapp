dc0d245529858aea47ceeb402f614974e4ae1ce2
daryl <tsdaryl@gmail.com>
2021-01-21 16:11:08 -0800

    added function to display error messages in HTML

--- express_server.js
+++ express_server.js
@@ -77,7 +77,9 @@ app.post("/urls/new", (req, res) => {
   const userID = req.session.user_id;
   const shortURL = generateRandomString(6);
   if (!userID) {
-    return res.redirect('/login');
+    templateVars['messageTitle'] = 'Login Required';
+    templateVars['message'] = 'Sorry, you have to be <a href="/login">logged in</a> to create a new URL.';
+    return res.render('urls_error', templateVars);
   }
   urlDatabase[shortURL] = { longURL, userID };
   res.redirect(`/urls/${shortURL}`);
@@ -101,23 +103,24 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const user = users[req.session.user_id];
   const target = urlDatabase[req.params.shortURL];
-  if (!user || !target || !checkOwnership(target, user)) {
-    console.log("You do not have the permission to edit a URL that belongs to someone else");
-    return res.redirect('/urls');
-  }
   const templateVars = getVars(req, urlDatabase, users);
+
+  if (!user) {
+    templateVars['messageTitle'] = 'Login Required';
+    templateVars['message'] = 'Sorry but you have to be <a href="/login">logged in</a> to create a new link.';
+    return res.render('urls_error', templateVars);
+  } else if (!target) {
+    templateVars['messageTitle'] = 'Short Link Not Found';
+    templateVars['message'] = 'The short link you requested could not be found.';
+    return res.render('urls_error', templateVars);
+  } else if (target.userID !== user.id) {
+    templateVars['messageTitle'] = 'Unauthorized';
+    templateVars['message'] = 'Sorry but you cannot edit a link that does not belong to you.';
+    return res.render('urls_error', templateVars);
+  }
+
   templateVars['target'] = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL].longURL};
   res.render("urls_show", templateVars);
-});
-
-
-
-app.get("/u/:shortURL", (req, res) => {
-  const target = urlDatabase[req.params.shortURL];
-  if (target === undefined) {
-    return res.redirect('/urls');
-  }
-  res.redirect(target.longURL);
 });
 
 
@@ -125,31 +128,61 @@ app.get("/u/:shortURL", (req, res) => {
 app.delete('/urls/:shortURL', (req, res) => {
   const user = users[req.session.user_id];
   const target = urlDatabase[req.params.shortURL];
-  if (!user || !target || !checkOwnership(target, user)) {
-    console.log("You do not have the permission to delete a URL that belongs to someone else");
-    return res.redirect('/urls');
+
+  if (!user) {
+    templateVars['messageTitle'] = 'Login Required';
+    templateVars['message'] = 'Sorry but you have to be <a href="/login">logged in</a> to create a new link.';
+    return res.render('urls_error', templateVars);
+  } else if (!target) {
+    templateVars['messageTitle'] = 'Short Link Not Found';
+    templateVars['message'] = 'The short link you requested could not be found.';
+    return res.render('urls_error', templateVars);
+  } else if (target.userID !== user.id) {
+    templateVars['messageTitle'] = 'Unauthorized';
+    templateVars['message'] = 'Sorry but you cannot edit a link that does not belong to you.';
+    return res.render('urls_error', templateVars);
   }
+
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
 });
 
 
 
-
 app.patch('/urls/:shortURL', (req, res) => {
   const user = users[req.session.user_id];
   const target = urlDatabase[req.params.shortURL];
-  if (target === undefined) {
-    console.log('Could not find this link.')
-    return res.redirect('/urls');
+  const templateVars = getVars(req, urlDatabase, users);
+  
+  if (!user) {
+    templateVars['messageTitle'] = 'Login Required';
+    templateVars['message'] = 'Sorry but you have to be <a href="/login">logged in</a> to create a new link.';
+    return res.render('urls_error', templateVars);
+  } else if (!target) {
+    templateVars['messageTitle'] = 'Short Link Not Found';
+    templateVars['message'] = 'The short link you requested could not be found.';
+    return res.render('urls_error', templateVars);
   } else if (target.userID !== user.id) {
-    console.log('Only the owner of this link can update its contents');
-    return res.redirect('/urls');
+    templateVars['messageTitle'] = 'Unauthorized';
+    templateVars['message'] = 'Sorry but you cannot edit a link that does not belong to you.';
+    return res.render('urls_error', templateVars);
   }
+
   urlDatabase[req.params.shortURL].longURL = req.body.newURL;
   res.redirect('/urls');
 });
 
+
+
+app.get("/u/:shortURL", (req, res) => {
+  const target = urlDatabase[req.params.shortURL];
+  if (!target) {
+    templateVars['messageTitle'] = 'Short Link Not Found';
+    templateVars['message'] = 'The short link you requested could not be found.';
+    return res.render('urls_error', templateVars);
+  }
+  res.redirect(target.longURL);
+});
 
 
 
@@ -162,10 +195,15 @@ app.get('/register', (req, res) => {
 
 
 app.post('/register', (req, res) => {
+  const templateVars = getVars(req, urlDatabase, users);
   if (!req.body.email || !req.body.password) {
-    console.log('No email or password provided');
+    templateVars['messageTitle'] = 'Incomplete Registration';
+    templateVars['message'] = 'You did not provide an email address or password. We need this information to log you in. Please try <a href="/register">signing up again</a>.';
+    return res.render('urls_error', templateVars);
   } else if (getUserByEmail(req.body.email, users)) {
-    console.log('Email address already in use');
+    templateVars['messageTitle'] = 'Email Address In Use';
+    templateVars['message'] = 'The email address you provided is already in use. Please try <a href="/login">logging in</a> or <a href="">sign up with a different email address</a>.';
+    return res.render('urls_error', templateVars);
   } else {
     const newUserID = generateRandomString(6);
     const newUserPw = bcrypt.hashSync(req.body.password, 10);
@@ -183,24 +221,35 @@ app.post('/register', (req, res) => {
 
 app.get('/login', (req, res) => {
   const templateVars = getVars(req, urlDatabase, users);
+  if (templateVars['user']) {
+    return res.redirect('/urls');
+  }
   res.render('urls_login', templateVars);
 });
 
 
 
 app.post('/login', (req, res) => {
+  const templateVars = getVars(req, urlDatabase, users);
   if (!req.body.email || !req.body.password) {
-    console.log("Can't let you in without the creds buddy.");
+    templateVars['messageTitle'] = 'Login Credentials Not Provided';
+    templateVars['message'] = 'You did not provide any login credentials';
+    return res.render('urls_error', templateVars);
   } else {
     const targetUser = getUserByEmail(req.body.email, users);
     if (!targetUser) {
-      console.log(`Nobody with ${req.body.email} exists.`);
+      templateVars['messageTitle'] = 'Email Does Not Exist';
+      templateVars['message'] = `The email address you provided (${req.body.email}) does not exist in our database.`;
+      return res.render('urls_error', templateVars);
     } else if (bcrypt.compareSync(req.body.password, targetUser.password)) {
       req.session.user_id = targetUser.id;
     } else {
-      console.log('Wrong password or some other error');
+      templateVars['messageTitle'] = 'Incorrect Password';
+      templateVars['message'] = 'The password you provided is incorrect. Please <a href="/login">try again</a>.';
+      return res.render('urls_error', templateVars);
     }
   }
+
   res.redirect('/urls');
 });
 
